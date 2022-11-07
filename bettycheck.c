5dd4ec2f9808cbc69b6028dfec9ac29e40838799
xjomino <jammwa@gmail.com>
2022-11-07 07:33:11 +0000

    betty

---
+++ bettycheck.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - multiplies two integers
+ * Return: (0) always
+ */
+int main(void)
+{
+	int k = 5;
+	int g = 10;
+	int mul;
+
+	mul = k * g;
+	printf("The multiplication of integers is:%d\n", mul);
+	return (0);
+}
