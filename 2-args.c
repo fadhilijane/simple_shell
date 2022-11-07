178eca5fc8ee81353740116916ce7bbc3ef90f94
xjomino <jammwa@gmail.com>
2022-11-07 18:18:41 +0000

    reading arguments

---
+++ 2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print the arguments
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	int k;
+
+	for (k = 0; k < argc; k++)
+	{
+		printf("%s\n", argv[k]);
+	}
+	return (0);
+}
