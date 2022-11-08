86f54cacbc513a774155485bf466b0bb83db22e4
Jane Fadhili <janefadhili@gmail.com>
2022-11-08 06:09:52 -0800

    prompt

---
+++ 1-readline.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints "$ "
+ * Return: 0
+ */
+
+int main(void)
+{
+	size_t n = 10;
+	char *buff = NULL;
+
+	while(1)
+	{
+		printf("$");
+		getline(&buff, &n, stdin);
+		printf("%s\n", buff);
+	}
+	free(buff);
+	return (0);
+}
